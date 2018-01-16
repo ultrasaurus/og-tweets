3e154fe7082bb51a3b867f6315c0c737b95e64ef
Sarah Allen <sarah@ultrasaurus.com>
2018-01-15 23:56:04 -0800

    really basic text summary, minor cleanup for readability

--- parse.js
+++ parse.js
@@ -1,23 +1,37 @@
 var fs = require("fs");
 
-var dataStr = fs.readFileSync("./all.json");
-var data = JSON.parse(dataStr);
-console.log("length:", data.length);
 
-
-function filterMentions(t) {
+function filterMentions(t, mentions) {
  // match @mentions separate from text at end of line
  var re = /(@[^\s]*|[^@ ].*$)/g
  var result = t.full_text.match(re)
+ if (result.length > 1) {
+   for (var i=0; i < result.length-2; i++) {
+     mentions[result[i]] = true;
+   }
+ }
+ t.short_text = result[result.length-1];
 }
 
 function printTweet(t) {
   var name = t.user['screen_name'];
-  console.log(name, ": ", t.full_text);
+  console.log(name + ": " + t.short_text );
   if (!name) console.log(t);
 }
 
-for (var i in data) {
-  printTweet(data[i]);
+function parseTweetFile(filePath) {
+  var dataStr = fs.readFileSync(filePath);
+  var data = JSON.parse(dataStr);
+  console.log(data.length + " tweets");
+
+  var mentions = {};
+  for (var i in data) {
+    filterMentions(data[i], mentions);
+    printTweet(data[i]);
+  }
+  var names = Object.keys(mentions);
+  console.log(names.length + ' mentions');
+  console.log(names.join(' '));
 }
 
+parseTweetFile("./replies-all.json");
